c0c2f60a8b6a48075f25c02d8360710a75cacd35
David Fialho <fialho.david@protonmail.com>
2017-12-06 10:18:59 +0000

    Add new tests for 'count_cycles()'

--- day06/test_day06_pt1.py
+++ day06/test_day06_pt1.py
@@ -19,6 +19,10 @@ def test_index_of_max(values: List[int], expected_index: int, expected_value: in
 
 @pytest.mark.parametrize("values, expected_cycles", [
     ([0, 2, 7, 0], 5),
+    ([0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11], 7864),
+    ([1], 1),
+    ([0], 1),
+    ([0, 0, 0], 1),
 ])
 def test_count_cycles(values: List[int], expected_cycles: int):
     assert expected_cycles == count_cycles(values)
