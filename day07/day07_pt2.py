bc7819b37b57f01eb133b6f4cb4e61172ec176f9
David Fialho <fialho.david@protonmail.com>
2017-12-08 12:17:04 +0000

    Refactor part 2 of day 07

--- day07/day07_pt2.py
+++ day07/day07_pt2.py
@@ -43,41 +43,51 @@ def tower_weight(program: Program) -> int:
 
 
 def read_tree(programs: Iterator[Tuple[str, int, List[str]]]) -> Program:
-    all_programs: Dict[str, Program] = {}
-    all_sub_programs: Dict[str, List[str]] = {}
+    # Stores all programs found in the input
+    all_programs: List[Program] = []
+    # Dictionary to map program names to actual programs
+    program_by_name: Dict[str, Program] = {}
+    # Stores a list of sub-program names for each program
+    sub_programs_by_name: Dict[str, List[str]] = {}
 
     for name, weight, sub_programs in programs:
-        all_programs[name] = Program(name, weight, [])
-        all_sub_programs[name] = sub_programs
+        program = Program(name, weight, [])
+        all_programs.append(program)
+        program_by_name[name] = program
+        sub_programs_by_name[name] = sub_programs
 
-    for program in all_programs.values():
-        for sub_program in all_sub_programs[program.name]:
-            program.sub_programs.append(all_programs[sub_program])
+    # Add the actual sub-programs to each program according to 'sub_programs_by_name'
+    for program in all_programs:
+        for sub_program in sub_programs_by_name[program.name]:
+            program.sub_programs.append(program_by_name[sub_program])
 
-    return all_programs[find_bottom_program(iter(all_programs.values()))]
+    return find_bottom_program(all_programs)
 
 
-def find_bottom_program(programs: Iterator[Program]) -> str:
-    all_programs: Set[str] = set()
-    all_sub_programs: Set[str] = set()
-
-    for program in programs:
-        all_programs.add(program.name)
-        all_sub_programs.update([sub_program.name for sub_program in program.sub_programs])
+def find_bottom_program(programs: Iterator[Program]) -> Program:
+    # Put all sub-programs in a set
+    sub_programs = set(sub_program for program in programs for sub_program in program.sub_programs)
 
     # If the input is correct the result of this difference should contain a single program,
     # the bottom program
-    bottom_program = all_programs - all_sub_programs
+    bottom_program = set(programs) - sub_programs
     return bottom_program.pop()
 
 
 def programs(path: str) -> Iterator[Tuple[str, int, List[str]]]:
+    """
+    Parses each line in the input. A line is converted to a tuple containing (1) the program name,
+    (2) the program weight, and (3) a list with the names of the sub-programs. This list is empty
+    if the program has no sub_programs.
+
+    :return: tuple with the program name, weight, an list of sub-programs' names
+    """
     with open(path) as file:
         for line in file:
-            program_params, *sub_programs = line.split("->")
-            name, weight = program_params.strip().split(" ")
+            program_attributes, *sub_programs = line.split("->")
+            name, weight = program_attributes.strip().split(" ")
 
-            # Remove parenthesis
+            # Remove parenthesis and convert to integer
             weight = int(weight[1:-1])
 
             if sub_programs:
@@ -87,7 +97,7 @@ def programs(path: str) -> Iterator[Tuple[str, int, List[str]]]:
 
 
 def main():
-    tower_weight(read_tree(programs("input.txt")))
+    tower_weight(read_tree(programs("input_example.txt")))
 
 
 if __name__ == '__main__':
