1cedfe96a4c338a90bf7101fe6d85e38b5e946e9
David Fialho <fialho.david@protonmail.com>
2017-12-12 15:28:04 +0000

    Refactor solution for day 10

--- day10/day10.py
+++ day10/day10.py
@@ -4,8 +4,8 @@ from typing import Iterator, List, Iterable
 
 
 def slide(elements: List[int], offset: int) -> List[int]:
-    bound = offset % len(elements)
-    return elements[bound:] + elements[:bound]
+    offset %= len(elements)
+    return elements[offset:] + elements[:offset]
 
 
 def reverse(elements: List[int], length: int) -> List[int]:
@@ -17,41 +17,35 @@ def xor(iterable: Iterable[int]) -> int:
     return reduce(operator.xor, iterable)
 
 
-def hash_round(lengths: Iterator[int], elements: List[int], position: int = 0, skip_size: int = 0):
-    # Slide elements to current position
-    elements = slide(elements, position)
-
-    for length in lengths:
-        elements = reverse(elements, length)
-        elements = slide(elements, length + skip_size)
-        position = (position + length + skip_size) % len(elements)
-        skip_size += 1
-
-    # Adjust the elements to their expected positions
-    elements = slide(elements, -position)
-
-    return elements, position, skip_size
-
-
 def hex_str(value: int) -> str:
     return f'{value:0{2}x}'
+
+
+def hash_rounds(lengths: Iterator[int], elements: List[int], count: int = 1):
+    position, skip_size = 0, 0
+    for i in range(count):
+        for length in lengths:
+            elements = reverse(elements, length)
+            elements = slide(elements, length + skip_size)
+            position = (position + length + skip_size) % len(elements)
+            skip_size += 1
+
+    # Adjust the elements to their expected positions
+    return slide(elements, -position)
 
 
 def knot_hash(input_str: str) -> str:
     # Convert characters to their ascii codes
     lengths = [ord(character) for character in input_str]
+
     # Append fixed suffix
     lengths.extend([17, 31, 73, 47, 23])
 
-    position, skip_size = 0, 0
-    elements = [i for i in range(256)]
-
     # Compute sparse hash
-    for i in range(64):
-        elements, position, skip_size = hash_round(lengths, elements, position, skip_size)
+    sparse_hash = hash_rounds(lengths, elements=[i for i in range(256)], count=64)
 
     # Compute dense hash
-    dense_hash = [xor(elements[i * 16:i * 16 + 16]) for i in range(256 // 16)]
+    dense_hash = [xor(sparse_hash[i * 16:i * 16 + 16]) for i in range(256 // 16)]
 
     # Convert to hexadecimal string
     hash_str = "".join(hex_str(value) for value in dense_hash)
@@ -73,8 +67,8 @@ def main():
     # Part 1
     #
 
-    elements, *_ = hash_round(lengths_from(input("input.txt")), elements=[i for i in range(256)])
-    print("Solution part 1:", elements[0] * elements[1])
+    sparse_hash = hash_rounds(lengths_from(input("input.txt")), [i for i in range(256)], count=1)
+    print("Solution part 1:", sparse_hash[0] * sparse_hash[1])
 
     #
     # Part 2
