41dd8807ceadec7c0b525781d464a2a9299cbfa5
David Fialho <fialho.david@protonmail.com>
2017-12-15 13:40:55 +0000

    Add solution for part 2 of day 15

--- day15/day15.py
+++ day15/day15.py
@@ -1,20 +1,21 @@
-from typing import Tuple, Iterator
+from typing import Tuple, Iterator, Optional
 
 
-def judge(generator_a: Iterator[int], generator_b: Iterator[int]):
+def judge(generator_a: Iterator[int], generator_b: Iterator[int], n_pairs: int):
     count = 0
-    for i in range(40000000):
+    for i in range(n_pairs):
         if next(generator_a) & 0xFFFF == next(generator_b) & 0xFFFF:
             count += 1
 
     return count
 
 
-def generator(seed: int, factor: int) -> Iterator[int]:
+def generator(seed: int, factor: int, multiple: Optional[int] = None) -> Iterator[int]:
     value = seed
     while True:
         value = (value * factor) % 2147483647
-        yield value
+        if not multiple or value % multiple == 0:
+            yield value
 
 
 def input(path: str) -> Tuple[int, int]:
@@ -30,8 +31,19 @@ def main():
     #
     seed_a, seed_b = input("input.txt")
     print("Solution part 1:", judge(
-        generator(seed_a, factor=16807),
-        generator(seed_b, factor=48271)
+        generator_a=generator(seed_a, factor=16807),
+        generator_b=generator(seed_b, factor=48271),
+        n_pairs=40000000
+    ))
+
+    #
+    # Part 2
+    #
+    seed_a, seed_b = input("input.txt")
+    print("Solution part 2:", judge(
+        generator_a=generator(seed_a, factor=16807, multiple=4),
+        generator_b=generator(seed_b, factor=48271, multiple=8),
+        n_pairs=5000000
     ))
 
 
--- day15/test_day15.py
+++ day15/test_day15.py
@@ -6,8 +6,20 @@ from day15 import judge, generator
 @pytest.mark.parametrize("seed_a, seed_b, match_count", [
     (65, 8921, 588)
 ])
-def test_count_used(seed_a: int, seed_b: int, match_count: int):
+def test_judge_without_multiple(seed_a: int, seed_b: int, match_count: int):
     assert match_count == judge(
         generator(seed_a, factor=16807),
-        generator(seed_b, factor=48271)
+        generator(seed_b, factor=48271),
+        n_pairs=40000000
+    )
+
+
+@pytest.mark.parametrize("seed_a, seed_b, match_count", [
+    (65, 8921, 309)
+])
+def test_judge_with_multiple(seed_a: int, seed_b: int, match_count: int):
+    assert match_count == judge(
+        generator(seed_a, factor=16807, multiple=4),
+        generator(seed_b, factor=48271, multiple=8),
+        n_pairs=5000000
     )
