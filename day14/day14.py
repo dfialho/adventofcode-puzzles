4a8091754c7b35b5f17408789d80152a34dd75b7
David Fialho <fialho.david@protonmail.com>
2017-12-15 12:26:03 +0000

    Add solution for part 2 of day 14

--- day14/day14.py
+++ day14/day14.py
@@ -1,5 +1,4 @@
-import random
-from typing import List, Iterator
+from typing import List, Iterator, Dict, Tuple, NewType, Optional
 
 import hashing
 
@@ -11,6 +10,74 @@ def hashes(key: str, n: int) -> Iterator[str]:
 
 def to_bits(hash_str: str) -> str:
     return "".join(f"{int(character, 16):04b}" for character in hash_str)
+
+
+Position = NewType('Position', Tuple[int, int])
+Group = NewType('Group', int)
+
+
+class Grid:
+
+    def __init__(self, size: int):
+        self._groups: List[List[int]] = [[None] * size for i in range(size)]
+
+    def __getitem__(self, position: Position):
+        x, y = position
+
+        if x >= 128 or x < 0 or y >= 128 or y < 0:
+            return None
+
+        return self._groups[y][x]
+
+    def __setitem__(self, position: Position, value: int):
+        x, y = position
+        self._groups[y][x] = value
+
+
+def min_optional(a: Optional[int], b: Optional[int]) -> Optional[int]:
+    if a is None:
+        return b
+    elif b is None:
+        return a
+    else:
+        return min(a, b)
+
+
+def count_groups(key: str) -> int:
+    groups: Dict[Group, List[Position]] = {}
+    group_grid = Grid(size=128)
+    next_group = 1
+
+    for y, hash_str in enumerate(hashes(key, n=128)):
+        for x, bit in enumerate(to_bits(hash_str)):
+            if bit == '0':
+                continue
+
+            left = group_grid[x - 1, y]
+            top = group_grid[x, y - 1]
+
+            if not left and not top:
+                # This is a new group
+                group_grid[x, y] = next_group
+                groups[next_group] = [(x, y)]
+                next_group += 1
+
+            else:
+                group = min_optional(left, top)
+                group_grid[x, y] = group
+                groups[group].append((x, y))
+
+                if left and top and left != top:
+                    # All positions in group with highest ID need to moved to group with lowest ID
+                    lowest_group = min(left, top)
+                    highest_group = max(left, top)
+
+                    for position in groups[highest_group]:
+                        group_grid[position] = lowest_group
+                        groups[lowest_group].append(position)
+                    del groups[highest_group]
+
+    return len(groups)
 
 
 def build_grid(key: str) -> List[str]:
@@ -35,10 +102,7 @@ def main():
     #
     # Part 2
     #
-
-    for i in range(5):
-        value = random.randint(1, 16)
-        print(f"{value:05b}")
+    print("Solution part 1:", count_groups(input("input.txt")))
 
 
 if __name__ == '__main__':
--- day14/test_day14.py
+++ day14/test_day14.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day14 import build_grid, count_used
+from day14 import build_grid, count_used, count_groups
 
 
 @pytest.mark.parametrize("key_str, expected_count", [
@@ -8,3 +8,10 @@ from day14 import build_grid, count_used
 ])
 def test_count_used(key_str: str, expected_count: int):
     assert expected_count == count_used(build_grid(key_str))
+
+
+@pytest.mark.parametrize("key_str, expected_count", [
+    ("flqrgnkx", 1242)
+])
+def test_count_regions(key_str: str, expected_count: int):
+    assert expected_count == count_groups(key_str)
